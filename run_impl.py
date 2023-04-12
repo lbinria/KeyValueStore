7b744b1418d14a1c016a92176c200342f176836a
lbinria <benjamin.loillier@inria.fr>
2023-04-12 16:57:51 +0200

    Improve run script

--- run_impl.py
+++ run_impl.py
@@ -3,13 +3,16 @@ import time
 import signal
 from subprocess import Popen
 
-print("--- Run Key Value Store implementation ---")
-impl_process = Popen([
-    "/usr/lib/jvm/jdk-19/bin/java",
-    "-jar",
-    "target/KeyValueStore-1.0-jar-with-dependencies.jar"
-    ])
+def run():
+    impl_process = Popen([
+        "/usr/lib/jvm/jdk-19/bin/java",
+        "-jar",
+        "target/KeyValueStore-1.0-jar-with-dependencies.jar"
+        ])
 
-# Wait implementation is terminated
-impl_process.wait()
-impl_process.terminate()
+    # Wait implementation is terminated
+    impl_process.wait()
+    impl_process.terminate()
+
+if __name__ == "__main__":
+    run()
